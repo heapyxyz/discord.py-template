6fd652651f68dd8a0995e6a6e7d89d642e67fb38
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-06-16 13:19:11 +0200

    feat: use `discord.Intents.all()` instead

--- src/bot.py
+++ src/bot.py
@@ -5,11 +5,7 @@ import glob
 
 class Bot(commands.Bot):
     def __init__(self):
-        intents = discord.Intents.default()
-        intents.presences = True
-        intents.members = True
-        intents.message_content = True
-
+        intents = discord.Intents.all()
         super().__init__(command_prefix="/", intents=intents)
 
     async def on_ready(self):
